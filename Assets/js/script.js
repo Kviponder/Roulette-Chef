ef1b4fcdbab27af4855a3117e6ce9bca6ad4ac01
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-03-28 21:26:18 -0400

    Updated readME

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,12 +5,10 @@
 // 		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 // 	}
 // };
-
 // fetch('https://tasty.p.rapidapi.com/recipes/auto-complete?prefix=chicken%20soup', options)
 // 	.then(response => response.json())
 // 	.then(response => console.log(response))
 // 	.catch(err => console.error(err));
-
 // const options = {
 //     method: 'GET',
 //     headers: {
@@ -18,7 +16,6 @@
 //         'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
 //     }
 // };
-
 // fetch('https://tasty.p.rapidapi.com/recipes/get-more-info?id=8138', options)
 //     .then(response => response.json())
 //     .then(response => console.log(response))
