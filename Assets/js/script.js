21fab5018e0096100398a95009f7bd519f231487
jacksonhanks <jacksonjohnrayhanks@gmail.com>
2023-04-06 19:34:48 -0400

    Added new API key and beginnings of random recipe

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,16 @@
 //New API Key
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f466c28a93mshc3c44718ab029c4p1af359jsnefa63d91cf93',
+		'X-RapidAPI-Host': 'themealdb.p.rapidapi.com'
+	}
+};
+
+fetch('https://themealdb.p.rapidapi.com/random.php', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 // const options = {
 // 	method: 'GET',
 // 	headers: {
@@ -17,25 +29,31 @@
 //Some things to be done I added asterisks just so its easier to look and see what needs done to what part:
 //Add an *event listener* to the *search form* that *fetches recipe results* from an API
 
+
+
 //Display the results of the API fetch in the *resultsList element*
 
 //Add an *event listener* to the *sign-in form* that *handles authentication* and *toggles the showNone* class to *display the saved recipes list*.
 //Add an *event listener* to the *Random Button* that *fetches a random recipe* from the API and *displays* it on the page
+const randomRecipeBtn = document.getElementById('getRandom');
+const randomContainer = document.getElementById('randomRecipe');
+
+
 //Add an *event listener* to each *saved recipe item* that *fetches the recipe details and *displays* them on a *separate page*.
 
 
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "f466c28a93mshc3c44718ab029c4p1af359jsnefa63d91cf93",
-    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-  },
-};
+//const options = {
+//  method: "GET",
+//  headers: {
+//    "X-RapidAPI-Key": "f466c28a93mshc3c44718ab029c4p1af359jsnefa63d91cf93",
+//    "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+//  },
+//};
 
-fetch("https://tasty.p.rapidapi.com/tags/list", options)
-  .then((response) => response.json())
-  .then((response) => console.log(response))
-  .catch((err) => console.error(err));
+//fetch("https://tasty.p.rapidapi.com/tags/list", options)
+//  .then((response) => response.json())
+//  .then((response) => console.log(response))
+//  .catch((err) => console.error(err));
 
 // let password = "";
 // for (let i = 0; i < length; i++) {
