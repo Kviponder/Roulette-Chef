1cb1ea95339d5de6f253660f5d52ea159165204e
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-04-10 16:31:18 -0400

    Prettier Formatting

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,65 +1,62 @@
-const randomRecipeBtn = document.getElementById('getRandom');
-const randomRecipeContainer = document.getElementById('randomRecipe');
+const randomRecipeBtn = document.getElementById("getRandom");
+const randomRecipeContainer = document.getElementById("randomRecipe");
 
 function createRandomRecipe(meal) {
-const {strMeal, strMealThumb, strInstructions} = meal;
-//Generate the elements that will apear on the card
-const recipeCard = document.createElement('div');
-  recipeCard.classList.add('recipeCard');
+  const { strMeal, strMealThumb, strInstructions } = meal;
+  //Generate the elements that will apear on the card
+  const recipeCard = document.createElement("div");
+  recipeCard.classList.add("recipeCard");
 
-  const recipeCardImg = document.createElement('div');
-  recipeCardImg.classList.add('recipeCard-img');
+  const recipeCardImg = document.createElement("div");
+  recipeCardImg.classList.add("recipeCard-img");
 
-  const img = document.createElement('img');
+  const img = document.createElement("img");
   img.src = strMealThumb;
   img.alt = strMeal;
 
-  const recipeCardDetails = document.createElement('div');
-  recipeCardDetails.classList.add('recipeCard-details');
+  const recipeCardDetails = document.createElement("div");
+  recipeCardDetails.classList.add("recipeCard-details");
 
-  const h2 = document.createElement('h2');
+  const h2 = document.createElement("h2");
   h2.textContent = strMeal;
 
-  const instructions = document.createElement('p');
+  const instructions = document.createElement("p");
   instructions.textContent = strInstructions;
-//Assembles HTML
-recipeCardImg.appendChild(img);
-recipeCardDetails.appendChild(h2);
-recipeCardDetails.appendChild(instructions);
-recipeCard.appendChild(recipeCardImg);
-recipeCard.appendChild(recipeCardDetails);
+  //Assembles HTML
+  recipeCardImg.appendChild(img);
+  recipeCardDetails.appendChild(h2);
+  recipeCardDetails.appendChild(instructions);
+  recipeCard.appendChild(recipeCardImg);
+  recipeCard.appendChild(recipeCardDetails);
 
-//Clear eexisting recipe details
-randomRecipeContainer.innerHTML = '';
+  //Clear eexisting recipe details
+  randomRecipeContainer.innerHTML = "";
 
-//Add card to page
-randomRecipeContainer.appendChild(recipeCard);
+  //Add card to page
+  randomRecipeContainer.appendChild(recipeCard);
 }
-randomRecipeBtn.addEventListener('click', () => {
-	fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-		.then(response => response.json())
-		.then(response => {
-			createRandomRecipe(response.meals[0]);
-		})
-		.catch(e => {
-			console.warn(e);
-		});
+randomRecipeBtn.addEventListener("click", () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((response) => response.json())
+    .then((response) => {
+      createRandomRecipe(response.meals[0]);
+    })
+    .catch((e) => {
+      console.warn(e);
+    });
 });
 
-const randomBtn = document.querySelector('.randomBtn');
+const randomBtn = document.querySelector(".randomBtn");
 
-randomBtn.addEventListener('mouseenter', () => {
-  randomBtn.style.setProperty('--color-r', Math.floor(Math.random() * 255));
-  randomBtn.style.setProperty('--color-g', Math.floor(Math.random() * 255));
-  randomBtn.style.setProperty('--color-b', Math.floor(Math.random() * 255));
-})
-
-
-
+randomBtn.addEventListener("mouseenter", () => {
+  randomBtn.style.setProperty("--color-r", Math.floor(Math.random() * 255));
+  randomBtn.style.setProperty("--color-g", Math.floor(Math.random() * 255));
+  randomBtn.style.setProperty("--color-b", Math.floor(Math.random() * 255));
+});
 
 // Set the canvas width and height
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 // Set the canvas width and height
 canvas.width = window.innerWidth;
@@ -109,7 +106,7 @@ function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   // Loop through the balls array and update each ball's position
-  balls.forEach(ball => {
+  balls.forEach((ball) => {
     ball.draw();
     ball.update();
   });
@@ -119,46 +116,46 @@ function animate() {
 }
 
 // Load saved balls from local storage if any
-if (localStorage.getItem('balls')) {
-  balls = JSON.parse(localStorage.getItem('balls'));
-  balls.forEach(ball => {
+if (localStorage.getItem("balls")) {
+  balls = JSON.parse(localStorage.getItem("balls"));
+  balls.forEach((ball) => {
     ball.__proto__ = Ball.prototype;
   });
 }
 
 // Add a new ball to the array each time the button is clicked
-document.getElementById('getRandom').addEventListener('click', () => {
+document.getElementById("getRandom").addEventListener("click", () => {
   const x = Math.random() * canvas.width;
   const y = Math.random() * canvas.height;
   const vx = Math.random() * 10 - 5;
   const vy = Math.random() * 10 - 5;
   const radius = Math.random() * 50 + 10;
-  const color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
   balls.push(new Ball(x, y, vx, vy, radius, color));
-  localStorage.setItem('balls', JSON.stringify(balls));
+  localStorage.setItem("balls", JSON.stringify(balls));
 });
 
 // Start the animation loop
 animate();
 
 // Display ball count
-const ballCount = document.getElementById('ballCount');
+const ballCount = document.getElementById("ballCount");
 ballCount.textContent = balls.length;
 
 // Increase ball count when a new ball is added
-document.getElementById('getRandom').addEventListener('click', () => {
+document.getElementById("getRandom").addEventListener("click", () => {
   ballCount.textContent = balls.length;
 });
 
 // Clear saved balls and ball count when the clear button is clicked
-document.getElementById('clearBtn').addEventListener('click', () => {
+document.getElementById("clearBtn").addEventListener("click", () => {
   balls = [];
-  localStorage.removeItem('balls');
+  localStorage.removeItem("balls");
   ballCount.textContent = 0;
 });
 
-const ballCountElem = document.getElementById('ballCount');
+const ballCountElem = document.getElementById("ballCount");
 
 function updateBallCount() {
   ballCountElem.innerText = balls.length.toString();
